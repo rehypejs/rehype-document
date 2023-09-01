b3b7d854f19087c55047dd368e72fa21d456ee9a
Titus Wormer <tituswormer@gmail.com>
2023-09-01 17:33:41 +0200

    Fix declaration merging

--- index.d.ts
+++ index.d.ts
@@ -2,26 +2,25 @@ export type {Options} from './lib/index.js'
 
 export {default} from './lib/index.js'
 
-declare module 'vfile' {
-  interface DataMap {
-    matter: {
-      /**
-       * Title of this document.
-       *
-       * Populated by `vfile-matter` from frontmatter; used by
-       * `rehype-document` and `rehype-meta`.
-       */
-      title?: string
-    }
+interface DocumentFields {
+  /**
+   * Title of the document (optional, example: `'The New York City Subway Map
+   * as You’ve Never Seen It Before'`).
+   *
+   * Inferred by `rehype-infer-title-meta` from HTML or `vfile-matter` from
+   * frontmatter.
+   * Used by `rehype-document` and `rehype-meta`.
+   */
+  title?: string | null | undefined
+}
 
-    meta: {
-      /**
-       * Title of this document.
-       *
-       * Populated by `rehype-infer-title-meta` from the HTML; used by
-       * `rehype-document` and `rehype-meta`.
-       */
-      title?: string
-    }
+// Add custom data supported when `rehype-document` is added.
+declare module 'vfile' {
+  interface DataMapMatter extends DocumentFields {}
+  interface DataMapMeta extends DocumentFields {}
+
+  interface DataMap {
+    matter: DataMapMeta
+    meta: DataMapMatter
   }
 }
