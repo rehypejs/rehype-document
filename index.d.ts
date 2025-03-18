8fbada91f5f69a89b56201063b257a6d246c2c27
Titus Wormer <tituswormer@gmail.com>
2025-03-18 10:40:06 +0100

    Refactor JSDocs

--- index.d.ts
+++ index.d.ts
@@ -7,8 +7,9 @@ export {default} from './lib/index.js'
  */
 interface DocumentFields {
   /**
-   * Title of the document (optional, example: `'The New York City Subway Map
-   * as You’ve Never Seen It Before'`).
+   * Title of the document
+   * (optional,
+   * example: `'The New York City Subway Map as You’ve Never Seen It Before'`).
    *
    * Inferred by `rehype-infer-title-meta` from HTML or `vfile-matter` from
    * frontmatter.
@@ -25,55 +26,75 @@ export interface Options {
    * URLs to stylesheets to use in `<link>`s (optional).
    */
   css?: ReadonlyArray<string> | string | null | undefined
+
   /**
    * Direction of the document (optional).
    */
   dir?: 'auto' | 'ltr' | 'rtl' | null | undefined
+
   /**
    * URLs to scripts to use as `src` on `<script>`s (optional).
    */
   js?: ReadonlyArray<string> | string | null | undefined
+
   /**
-   * Language of document (default: `'en'`); should be a
-   * [BCP 47](https://tools.ietf.org/html/bcp47) language tag.
+   * Language of document
+   * (default: `'en'`);
+   * should be a [BCP 47](https://tools.ietf.org/html/bcp47) language tag.
    */
   language?: string | null | undefined
+
   /**
-   * Generate extra `<link>`s with these properties (optional); passed as
-   * `properties` to [`hastscript`](https://github.com/syntax-tree/hastscript)
-   * with `'link'`.
+   * Generate extra `<link>`s with these properties
+   * (optional);
+   * passed as `properties` to
+   * [`hastscript`](https://github.com/syntax-tree/hastscript) with `'link'`.
    */
   link?:
     | ReadonlyArray<Readonly<Properties>>
     | Readonly<Properties>
     | null
     | undefined
+
   /**
-   * Generate extra `<meta>`s with these properties (optional); passed as
-   * `properties` to [`hastscript`](https://github.com/syntax-tree/hastscript)
-   * with `'meta'`.
+   * Generate extra `<meta>`s with these properties
+   * (optional);
+   * passed as `properties` to
+   * [`hastscript`](https://github.com/syntax-tree/hastscript) with `'meta'`.
    */
   meta?:
     | ReadonlyArray<Readonly<Properties>>
     | Readonly<Properties>
     | null
     | undefined
+
   /**
-   * Generate a `meta[viewport]` (default: `true`).
+   * Generate a `meta[viewport]`
+   * (default: `true`).
    */
   responsive?: boolean | null | undefined
+
   /**
-   * JavaScript source code of `<script>`s to add at end of `body` (optional).
+   * JavaScript source code of `<script>`s to add at end of `body`
+   * (optional).
    */
   script?: ReadonlyArray<string> | string | null | undefined
+
   /**
-   * CSS source code of `<style>`s to add (optional).
+   * CSS source code of `<style>`s to add
+   * (optional).
    */
   style?: ReadonlyArray<string> | string | null | undefined
+
   /**
-   * Text to use as title (optional); defaults to the file name (if any); can
-   * bet set with `file.data.matter.title` (`vfile-matter`) and
-   * `file.data.meta.title` (`rehype-infer-title-meta`), which are preferred.
+   * Text to use as title
+   * (optional);
+   * defaults to the file name (if any);
+   * can bet set with `file.data.matter.title`
+   * (`vfile-matter`)
+   * and `file.data.meta.title`
+   * (`rehype-infer-title-meta`),
+   * which are preferred.
    */
   title?: string | null | undefined
 }
